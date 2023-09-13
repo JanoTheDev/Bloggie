94b98fde224df2d333ddeea8746deec19594903a
JanoTheDev <64414083+JanoTheDev@users.noreply.github.com>
2023-09-13 17:13:04 +0300

    Added loading state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,30 @@ import SideBar from "@/components/Navbar";
 import SmallCardInfo from "@/components/SmallCardInfo";
 import { BlogData } from "@/data/BlogData";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <div>
-      <SideBar
-        children={
-          <div className="items-center justify-center flex pt-6 lg:pt-0 lg:justify-start lg:items-start pb-24">
-            <div className="flex flex-wrap justify-center gap-3">
-              {BlogData.map((x: any, i: number) => (
-                <SmallCardInfo data={x} />
-              ))}
+      {loading === true ? (
+        <></>
+      ) : (
+        <SideBar
+          children={
+            <div className="items-center justify-center flex pt-6 lg:pt-0 lg:justify-start lg:items-start pb-24">
+              <div className="flex flex-wrap justify-center gap-3">
+                {BlogData.map((x: any, i: number) => (
+                  <SmallCardInfo data={x} />
+                ))}
+              </div>
             </div>
-          </div>
-        }
-      />
+          }
+        />
+      )}
     </div>
   );
 }

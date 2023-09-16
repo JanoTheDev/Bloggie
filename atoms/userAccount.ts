cc9be8cf8c81d0c001eda5e31c1374c09475333a
JanoTheDev <64414083+JanoTheDev@users.noreply.github.com>
2023-09-16 20:43:14 +0300

    Update userAccount.ts

--- atoms/userAccount.ts
+++ atoms/userAccount.ts
@@ -1,5 +1,4 @@
 import { atom } from "jotai";
-import { userData } from "@/data/UserData";
 import { AllUserData } from "@/data/AllUserData";
 
 export const userAccount = atom<any>(AllUserData[1]);
